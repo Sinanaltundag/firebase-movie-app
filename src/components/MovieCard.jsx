5c4aa7bb814676857e61b05fc27938a7bc47e092
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-04 02:50:19 +0300

    reduce image size

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const MovieCard = ({title, poster_path,overview, vote_average,id}) => {
+const MovieCard = ({title, backdrop_path, overview, vote_average,id}) => {
 
   const toastId = React.useRef(null);
   
@@ -21,7 +21,7 @@ const {currentUser} = useContext(AuthContext)
 let navigate = useNavigate()
   return (
     <div className="movie" onClick={currentUser? ()=> navigate("details/"+id):loginFirst} >
-<img src={poster_path?IMG_API+poster_path:defaultImage} alt="" />
+<img src={backdrop_path?IMG_API+backdrop_path:defaultImage} alt="" />
 <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
     <h5>{title}</h5>
     {currentUser&&<span className={`tag ${vote_average>=8?"green": vote_average>=6?"orange":"red"}`}>{vote_average}</span>}
